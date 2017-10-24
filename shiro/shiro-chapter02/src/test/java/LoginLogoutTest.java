8184dd3a3a7dbd32d3f4d3969b0b8dc1895570bf
yangdd1205 <yangdd1205@126.com>
2017-10-24 18:29:27 +0800

    :sparkles: shiro chapter02

---
+++ shiro/shiro-chapter02/src/test/java/LoginLogoutTest.java
@@ -0,0 +1,38 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginLogoutTest {
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        //1.获取SecurityManager工厂，此处使用ini文件初始化SecurityManager
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+
+        //2.得到SecurityManager实例，并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3.得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        try {
+            //4.登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5.验证失败
+        }
+        //断言用户已经登录
+        Assert.assertEquals(true, subject.isAuthenticated());
+
+        //6.退出
+        subject.logout();
+    }
+}
