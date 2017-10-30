754ef9b0182896b2b0aeb0def47f64b14fd64d5e
yangdd1205 <yangdd1205@126.com>
2017-10-30 16:47:07 +0800

    :sparkles: shiro chapter03 authorization

---
+++ shiro/shiro-chapter03-authorization/src/test/java/permission/MyRolePermissionResolver.java
@@ -0,0 +1,19 @@
+package permission;
+
+import org.apache.shiro.authz.Permission;
+import org.apache.shiro.authz.permission.RolePermissionResolver;
+import org.apache.shiro.authz.permission.WildcardPermission;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MyRolePermissionResolver implements RolePermissionResolver{
+    @Override
+    public Collection<Permission> resolvePermissionsInRole(String roleString) {
+        if("role1".equals(roleString)){
+            return Arrays.asList(new WildcardPermission("menu:*"));
+        }
+        return null;
+
+    }
+}
