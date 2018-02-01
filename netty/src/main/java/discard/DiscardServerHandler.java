e423330419b90ca820390204b4354c07758cad18
yangdd1205 <yangdd1205@126.com>
2018-02-02 06:35:54 +0800

    :sparkles: netty discard

---
+++ netty/src/main/java/discard/DiscardServerHandler.java
@@ -0,0 +1,56 @@
+package discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端 channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 每当从客户端收到新的数据时，这个方法会在收到消息时被调用，这个例子中，收到的消息的类型是 ByteBuf
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        /*
+
+        //默默地丢弃收到的数据
+        ((ByteBuf) msg).release();
+
+         */
+
+        /*
+
+        try {
+            // Do something with msg
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+
+        */
+
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+            //这个低效的循环事实上可以简化为:System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII))
+        } finally {
+            ReferenceCountUtil.release(in);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
