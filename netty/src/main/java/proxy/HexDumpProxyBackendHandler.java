2d9d7a657c5f69b546496c225347bf971119db8e
yangdd1205 <yangdd1205@126.com>
2018-02-12 08:14:01 +0800

    proxy

---
+++ netty/src/main/java/proxy/HexDumpProxyBackendHandler.java
@@ -0,0 +1,41 @@
+package proxy;
+
+import io.netty.channel.*;
+
+/**
+ */
+public class HexDumpProxyBackendHandler extends ChannelInboundHandlerAdapter {
+
+    private final Channel inboundChannel;
+
+    public HexDumpProxyBackendHandler(Channel inboundChannel) {
+        this.inboundChannel = inboundChannel;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.read();
+    }
+
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, Object msg) {
+        inboundChannel.writeAndFlush(msg).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                ctx.channel().read();
+            } else {
+                future.channel().close();
+            }
+        });
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        HexDumpProxyFrontendHandler.closeOnFlush(inboundChannel);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        HexDumpProxyFrontendHandler.closeOnFlush(ctx.channel());
+    }
+}
