e0aeb9abe7cdd99f1c1cfe81072d7f1344df820c
yangdd1205 <yangdd1205@126.com>
2018-02-25 18:07:28 +0800

    http file

---
+++ netty/src/main/java/http/file/HttpStaticFileServer.java
@@ -0,0 +1,42 @@
+package http.file;
+
+import http.helloworld.HttpHelloWorldServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+
+public final class HttpStaticFileServer {
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
+
+
+    public static void main(String[] args) throws Exception {
+        SslContext sslCtx = null;
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpStaticFileServerInitializer(sslCtx));
+
+            Channel channel = b.bind(PORT).sync().channel();
+
+
+            System.err.println("Open your web browser and navigate to " +
+                    (SSL ? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+
+            channel.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ netty/src/main/java/http/file/HttpStaticFileServerInitializer.java
@@ -0,0 +1,29 @@
+package http.file;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class HttpStaticFileServerInitializer extends ChannelInitializer<SocketChannel> {
+    private final SslContext sslCtx;
+
+    public HttpStaticFileServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (sslCtx != null) {
+            pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65535));
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpStaticFileServerHandler());
+    }
+}
