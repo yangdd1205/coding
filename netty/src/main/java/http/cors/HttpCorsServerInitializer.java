e250740ab208052fb6b41ff0a8d3672d0fb2bdc1
yangdd1205 <yangdd1205@126.com>
2018-03-03 13:05:51 +0800

    http cors

--- netty/src/main/java/http/cors/HttpCorsServerInitializer.java
+++ netty/src/main/java/http/cors/HttpCorsServerInitializer.java
@@ -2,16 +2,13 @@ package http.cors;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
-import io.netty.channel.ConnectTimeoutException;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpResponseDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.cors.CorsConfig;
 import io.netty.handler.codec.http.cors.CorsHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.stream.ChunkedWriteHandler;
+
 
 public class HttpCorsServerInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -23,13 +20,19 @@ public class HttpCorsServerInitializer extends ChannelInitializer<SocketChannel>
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-        CorsConfig corsConfig = CorsConfig.withAnyOrigin().build();
+        //允许任何来源
+        //CorsConfig corsConfig = CorsConfig.withAnyOrigin().build();
+        //指定来源
+        //CorsConfig corsConfig = CorsConfig.withOrigin("http://eureka1:1205").build();
+
+        //指定headers 只能比设置的少， 如果不设置则不能有任何的header
+        CorsConfig corsConfig = CorsConfig.withAnyOrigin().allowedRequestHeaders("custom-request-header").build();
         ChannelPipeline pipeline = ch.pipeline();
         if (sslCtx != null) {
             pipeline.addLast(sslCtx.newHandler(ch.alloc()));
         }
         pipeline.addLast(new HttpResponseEncoder());
-        pipeline.addLast(new HttpResponseDecoder());
+        pipeline.addLast(new HttpRequestDecoder());
         pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast(new ChunkedWriteHandler());
         pipeline.addLast(new CorsHandler(corsConfig));
--- netty/src/main/java/http/cors/OkResponseHandler.java
+++ netty/src/main/java/http/cors/OkResponseHandler.java
@@ -3,14 +3,20 @@ package http.cors;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.HttpResponse;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.*;
 
 public class OkResponseHandler extends SimpleChannelInboundHandler<Object> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object o) throws Exception {
+        if (o instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) o;
+            HttpHeaders headers = request.headers();
+            if (headers != null) {
+                headers.forEach(h->{
+                    System.out.println(h.getKey()+": "+h.getValue());
+                });
+            }
+        }
         final HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
         response.headers().set("custom-response-header", "Some value");
         ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
