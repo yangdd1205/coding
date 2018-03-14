2f30535e64740d3c94fef62fb80a54da75b183c9
yangdd1205 <yangdd1205@126.com>
2018-03-14 10:10:42 +0800

    uptime

---
+++ netty/src/main/java/uptime/UptimeServerHandler.java
@@ -0,0 +1,21 @@
+package uptime;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+@ChannelHandler.Sharable
+public class UptimeServerHandler extends SimpleChannelInboundHandler<Object>{
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
